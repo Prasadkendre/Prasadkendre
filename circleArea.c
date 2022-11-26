c71c32ebd9733a3c30cab212b20ed08f4af6a495
Prasadkendre <106261471+Prasadkendre@users.noreply.github.com>
2022-11-26 23:43:10 +0530

    Create circleArea.c
    Area of circle in C language .

---
+++ circleArea.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  float r,area ;
+  system("cls");
+  printf("\n\t Enter the radius of circle :- ");
+  scanf("%f",&r);
+  area = 3.14*3.14*r ;
+  printf("\n\t The area of circle is %d sq.cm",area); 
+  return 0;
+}  
