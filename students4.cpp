ec4a89f26ed93ab52a41ddb14849b55d246974e2
prasad <106261471+Prasadkendre@users.noreply.github.com>
2022-12-01 23:26:22 +0530

    Add files via upload
    any numbers of students data.

---
+++ students4.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std ;
+void Studentdata()
+{
+    char name[15] ;
+    int roll ;
+    float per ;
+    cout<<"\n\t name= ";
+    cin>>name ;
+    cout<<"\n\t Roll no.=" ;
+    cin>>roll ;
+    cout<<"\n\t percentage=" ;
+    cin>>per ;
+}
+
+void display()
+{
+    char name;
+    int roll ;
+    float per;
+    cout<<"\n\t name ="<<name ;
+    cout<<"\n\t roll no="<<roll ;
+    cout<<"\n\t percentage="<<per ;
+}
+int main()
+{
+    int n;
+    cout<<"\n\t enter the no. of student's data to be fill ";
+    cin>>n ;
+    int i ;
+    for (i = 1; i <n+1; i++)
+    {
+        cout<<"\n\tenter the details of student "<<i<<endl ;
+        Studentdata();
+        display();
+    }
+    
+
+    return 0;
+}
